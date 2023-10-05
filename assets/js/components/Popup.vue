689982f5c586e0a8be71c316f5ff3e37a2d6d8db
Molnar Mate <molnar.mate@evista.hu>
2023-10-06 00:33:34 +0200

    popup UI update

--- assets/js/components/Popup.vue
+++ assets/js/components/Popup.vue
@@ -1,21 +1,53 @@
 <template>
-  <div class="wrapper">
-    <h1 class="title">Hello World Notes</h1>
-    <div class="buttons">
-      <button type="button" class="px-2 py-1 bg-green-300">Btn1</button>
-      <button type="button" class="state-on">Btn1</button>
+  <div class="grid-col-12">
+    <div class="col-span-12 flex flex-row justify-center items-center my-2">
+      <img
+        src="./../../images/icon-128x128.png"
+        class="h-12 w-auto rounded-lg"
+        alt="logo"
+      />
+      <div class="flex flex-col items-start ml-2">
+        <h1 class="text-gray-800 font-bold">CoverSurface v1.0</h1>
+        <small class="text-gray-400 font-bold">Copyright - 2023</small>
+      </div>
     </div>
-    <div class="sites">
-      <p class="border border-red-500">{{ theTitle }}</p>
-      <textarea
-        rows="8"
-        autocomplete="off"
-        autocorrect="off"
-        autocapitalize="off"
-        spellcheck="false"
-      ></textarea>
+    <div
+      class="col-span-12 flex flex-row justify-center items-center gap-x-2 mb-2"
+    >
+      <span class="cursor-pointer rounded-md hover:bg-[#f9ceb2] p-1"
+        ><img src="./../../images/Info_icon.svg" alt="info_icon"
+      /></span>
+      <span class="cursor-pointer rounded-md hover:bg-[#f9ceb2] p-1"
+        ><img src="./../../images/On_button_icon.svg" alt="on_icon"
+      /></span>
+      <span class="cursor-pointer rounded-md hover:bg-[#f9ceb2] p-1"
+        ><img src="./../../images/Setting_line.svg" alt="settings_icon"
+      /></span>
     </div>
-    <button type="button" class="state-save">Example Submit Button</button>
+    <div
+      class="col-span-12 flex flex-col justify-center items-center py-2 px-5 border-gray-400 border-y"
+    >
+      <div class="flex flex-row justify-between w-full mx-2">
+        <p class="text-gray-400 font-bold">Nodes/Peers</p>
+        <p class="text-gray-800 font-bold">576</p>
+      </div>
+      <div class="flex flex-row justify-between w-full mx-2">
+        <p class="text-gray-400 font-bold">Gateway</p>
+        <p class="text-gray-800 font-bold">localhost:3054</p>
+      </div>
+      <div class="flex flex-row justify-between w-full mx-2">
+        <p class="text-gray-400 font-bold">API</p>
+        <p class="text-gray-800 font-bold">127.0.0.1:1633</p>
+      </div>
+    </div>
+    <div class="col-span-12 flex flex-col justify-center items-center py-2">
+      <button
+        class="flex flex-row justify-evenly items-center w-3/5 mx-4 py-1 rounded-md border border-[#f9ceb2] hover:bg-[#f9ceb2]"
+      >
+        <img src="./../../images/surface_icon.svg" alt="surface_icon" />
+        <h1 class="text-gray-800 font-bold">My Surface</h1>
+      </button>
+    </div>
   </div>
 </template>
 
